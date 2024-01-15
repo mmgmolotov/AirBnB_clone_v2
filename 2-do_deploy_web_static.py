cfa6c7891fbaa2def96004ea6a3d3570081e1870
mmgmolotov <mmgmolotov@gmail.com>
2024-01-15 20:25:56 +0100

    Refactor deployment script to improve error handling and readability

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 # Fabfile to distribute an archive to a web server.
 
 import os.path
-from fabric.api import env, put, run
+from fabric.api import env, put, run, abort
 
 env.user = "ubuntu"
 env.hosts = ['52.3.247.21', '34.207.237.255']
@@ -18,52 +18,69 @@ def do_deploy(archive_path):
         bool: True if successful, False otherwise.
     """
     if not os.path.isfile(archive_path):
-        print("Error: Specified archive file does not exist.")
-        return False
+        abort("Error: Specified archive file does not exist.")
 
     full_file = os.path.basename(archive_path)
     folder = os.path.splitext(full_file)[0]
 
     try:
-        # Uploads archive to /tmp/ directory
-        if put(archive_path, "/tmp/{}".format(full_file)).failed:
-            raise Exception("Uploading archive to /tmp/ failed")
-
-        # Clean up existing folder with the same name on the server
-        if run("rm -rf /data/web_static/releases/{}/".format(folder)).failed:
-            raise Exception("Deleting existing folder failed")
-
-        # Create a new archive folder
-        if run("sudo mkdir -p /data/web_static/releases/{}/".format(folder)).failed:
-            raise Exception("Creating new archive folder failed")
-
-        # Uncompress archive to /data/web_static/releases/{}/ directory
-        if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(full_file, folder)).failed:
-            raise Exception("Uncompressing archive failed")
-
-        # Clean up: delete the uploaded archive
-        if run("rm /tmp/{}".format(full_file)).failed:
-            raise Exception("Deleting archive from /tmp/ directory failed")
-
-        # Move content from web_static to its parent folder
-        if run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(folder, folder)).failed:
-            raise Exception("Moving content to archive folder failed")
-
-        # Clean up: delete the empty web_static folder
-        if run("rm -rf /data/web_static/releases/{}/web_static".format(folder)).failed:
-            raise Exception("Deleting web_static folder failed")
-
-        # Clean up: delete current folder (the symbolic link)
-        if run("rm -rf /data/web_static/current").failed:
-            raise Exception("Deleting 'current' folder failed")
-
-        # Create new symbolic link on the web server linked to the new code version
-        if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(folder)).failed:
-            raise Exception("Creating new symbolic link to new code version failed")
+        upload_archive(archive_path, full_file)
+        cleanup_existing_folder(folder)
+        create_new_archive_folder(folder)
+        uncompress_archive(full_file, folder)
+        delete_uploaded_archive(full_file)
+        move_content_to_archive_folder(folder)
+        delete_empty_web_static_folder(folder)
+        delete_current_folder()
+        create_new_symbolic_link(folder)
 
         print("New version deployed!")
         return True
 
     except Exception as e:
-        print("Error:", e)
-        return False
+        abort("Error: {}".format(e))
+
+
+def upload_archive(archive_path, full_file):
+    if put(archive_path, "/tmp/{}".format(full_file)).return_code != 0:
+        raise Exception("Uploading archive to /tmp/ failed")
+
+
+def cleanup_existing_folder(folder):
+    if run("rm -rf /data/web_static/releases/{}/".format(folder)).return_code != 0:
+        raise Exception("Deleting existing folder failed")
+
+
+def create_new_archive_folder(folder):
+    if run("mkdir -p /data/web_static/releases/{}/".format(folder)).return_code != 0:
+        raise Exception("Creating new archive folder failed")
+
+
+def uncompress_archive(full_file, folder):
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(full_file, folder)).return_code != 0:
+        raise Exception("Uncompressing archive failed")
+
+
+def delete_uploaded_archive(full_file):
+    if run("rm /tmp/{}".format(full_file)).return_code != 0:
+        raise Exception("Deleting archive from /tmp/ directory failed")
+
+
+def move_content_to_archive_folder(folder):
+    if run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(folder, folder)).return_code != 0:
+        raise Exception("Moving content to archive folder failed")
+
+
+def delete_empty_web_static_folder(folder):
+    if run("rm -rf /data/web_static/releases/{}/web_static".format(folder)).return_code != 0:
+        raise Exception("Deleting web_static folder failed")
+
+
+def delete_current_folder():
+    if run("rm -rf /data/web_static/current").return_code != 0:
+        raise Exception("Deleting 'current' folder failed")
+
+
+def create_new_symbolic_link(folder):
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(folder)).return_code != 0:
+        raise Exception("Creating new symbolic link to new code version failed")
