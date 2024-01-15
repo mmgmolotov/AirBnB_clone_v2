d206e8478dfde30b0d95712beee065004b25748b
mmgmolotov <mmgmolotov@gmail.com>
2024-01-15 03:01:16 +0100

    Refactor do_pack function to improve readability and error handling

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,12 +7,13 @@ import time
 def do_pack():
     """ Creates a compressed archive of web_static folder """
     try:
+        current_time = time.strftime('%Y%m%d%H%M%S')
+        filename = "web_static_{}.tgz".format(current_time)
         local("mkdir -p versions")
-        local(f"tar -cvzf versions/web_static_{time.strftime('%Y%m%d%H%M%S')}.tgz "
-              f"web_static/")
-        return f"versions/web_static_{time.strftime('%Y%m%d%H%M%S')}.tgz"
+        local("tar -cvzf versions/{} web_static/".format(filename))
+        return "versions/{}".format(filename)
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print("An error occurred: {}".format(e))
         return None
 
 # Calling the function
