4b905c3df56a207e156022ea386e1629dea42654
mmgmolotov <mmgmolotov@gmail.com>
2024-01-16 22:36:39 +0100

    Refactor do_deploy function to remove unnecessary sudo commands

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,34 @@
+#!/usr/bin/python3
+""" a Fabric script (based on the file 1-pack_web_static.py) that distributes..
+    ..an archive to your web servers, using the function do_deploy: """
+
+
 from fabric.api import *
 from datetime import datetime
 from os.path import exists
 
-env.hosts = ['52.3.247.21', '34.207.237.255']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+
+env.hosts = ['52.87.255.164', '18.204.11.196']
 
 def do_deploy(archive_path):
-    """Distributes an archive to my web servers."""
-    if not exists(archive_path):
+    """ distributes an archive to my web servers
+    """
+    if exists(archive_path) is False:
         return False
-
     filename = archive_path.split('/')[-1]
-    no_tgz = '/data/web_static/releases/{}'.format(filename.split('.')[0])
-    tmp = "/tmp/{}".format(filename)
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    tmp = "/tmp/" + filename
 
     try:
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}/".format(no_tgz))
-        run("sudo tar -xzf {} -C {}/".format(tmp, no_tgz))
-        run("sudo rm {}".format(tmp))
-        run("sudo mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-        run("sudo rm -rf {}/web_static".format(no_tgz))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {}/ /data/web_static/current".format(no_tgz))
+        run("mkdir -p {}/".format(no_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
 
         return True
-    except Exception as e:
-        print("Error: {}".format(e))
+    except:
         return False
