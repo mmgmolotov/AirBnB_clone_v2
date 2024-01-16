d532ea5eb10d18bf94301eaab752ae7cfa81119a
mmgmolotov <mmgmolotov@gmail.com>
2024-01-16 22:46:36 +0100

    Update do_deploy function to use sudo for certain commands

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,28 +3,29 @@ from datetime import datetime
 from os.path import exists
 
 env.hosts = ['52.3.247.21', '34.207.237.255']
-env.key_filename = "~/.ssh/school"  # Add this line to specify the private key file
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
 
 def do_deploy(archive_path):
-    """Distributes an archive to my web servers"""
+    """Distributes an archive to my web servers."""
     if not exists(archive_path):
         return False
 
     filename = archive_path.split('/')[-1]
-    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
-    tmp = "/tmp/" + filename
+    no_tgz = '/data/web_static/releases/{}'.format(filename.split('.')[0])
+    tmp = "/tmp/{}".format(filename)
 
     try:
         put(archive_path, "/tmp/")
-        run("mkdir -p {}/".format(no_tgz))
-        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
-        run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-        run("rm -rf {}/web_static".format(no_tgz))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+        run("sudo mkdir -p {}/".format(no_tgz))
+        run("sudo tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("sudo rm {}".format(tmp))
+        run("sudo mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("sudo rm -rf {}/web_static".format(no_tgz))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {}/ /data/web_static/current".format(no_tgz))
 
         return True
     except Exception as e:
-        print(f"Error: {e}")
+        print("Error: {}".format(e))
         return False
