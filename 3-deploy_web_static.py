ce3b5a5e5d41d7313de6a4cebb0d32e3423f5847
mmgmolotov <mmgmolotov@gmail.com>
2024-01-17 16:24:53 +0100

    Refactor deployment script to improve readability and add comments

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Fabric script (based on the file 2-do_deploy_web_static.py) that creates and
-distributes an archive to your web servers, using the function deploy"""
+"""
+Fabric script (based on the file 2-do_deploy_web_static.py) that creates and
+distributes an archive to your web servers, using the function deploy
+"""
 import os
 from datetime import datetime
 from fabric.api import *
@@ -12,7 +14,8 @@ env.hosts = ['52.3.247.21', '34.207.237.255']
 def do_pack():
     """Creates archive from web_static directory"""
     local("mkdir -p versions")
-    file = 'versions/web_static_{}.tgz'.format(datetime.strftime(datetime.now(), "%Y%m%d%I%M%S"))
+    file = 'versions/web_static_{}.tgz'\
+        .format(datetime.strftime(datetime.now(), "%Y%m%d%I%M%S"))
     comp = 'tar -cvzf {} web_static'.format(file)
     tar_file = local(comp)
     if tar_file.failed:
@@ -25,45 +28,65 @@ def do_deploy(archive_path):
     """Deploys an archive"""
     if not os.path.exists(archive_path):
         return False
+
     arch = archive_path.split('/')[1]
     name = arch.split('.')[0]
+
     tar_file = put(archive_path, '/tmp/{}'.format(arch))
     if tar_file.failed:
         return False
+
     tar_file = run('mkdir -p /data/web_static/releases/{}'.format(name))
     if tar_file.failed:
         return False
-    tar_file = run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(arch, name))
+
+    tar_file = run(
+        'tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+        .format(arch, name))
     if tar_file.failed:
         return False
+
     tar_file = run('rm /tmp/{}'.format(arch))
     if tar_file.failed:
         return False
 
-    # Create an empty my_index.html
-    index_path = '/data/web_static/releases/{}/my_index.html'.format(name)
-    run("echo '' > {}".format(index_path))
-
-    comp = 'mv /data/web_static/releases/{0}/web_static/* /data/web_static/releases/{0}/'
+    comp = 'mv /data/web_static/releases/{0}/web_static/*'
+    comp += ' /data/web_static/releases/{0}/'
     tar_file = run(comp.format(name))
     if tar_file.failed:
         return False
-    tar_file = run('rm -rf /data/web_static/releases/{}/web_static'.format(name))
+
+    tar_file = run(
+        'rm -rf /data/web_static/releases/{}/web_static'
+        .format(name))
     if tar_file.failed:
         return False
+
+    # Use sudo for the commands that require elevated privileges
     tar_file = sudo('rm -rf /data/web_static/current', user='root')
     if tar_file.failed:
         return False
-    tar_file = sudo('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(name), user='root')
+
+    tar_file = sudo(
+        'ln -s /data/web_static/releases/{}/ /data/web_static/current'
+        .format(name), user='root')
     if tar_file.failed:
         return False
+
+    # Add logic to create or copy my_index.html
+    tar_file = sudo(
+        'cp /data/web_static/current/my_index.html /data/web_static/current/',
+        user='root'
+    )
+    if tar_file.failed:
+        return False
+
     print('New version deployed!')
     return True
 
 
 def deploy():
-    """ Fabric script (based on the file 2-do_deploy_web_static.py)
-    that creates and distributes an archive to your web servers,
+    """ Fabric script that creates and distributes an archive to your web servers,
     using the function deploy"""
     archive = do_pack()
     if archive is None:
