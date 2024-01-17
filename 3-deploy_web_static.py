4d24906c63926dda464347553240dd0bf78ef30e
mmgmolotov <mmgmolotov@gmail.com>
2024-01-17 16:32:06 +0100

    Fix deployment script to create symbolic link correctly

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,91 +1,61 @@
 #!/usr/bin/python3
-"""
-Fabric script (based on the file 2-do_deploy_web_static.py) that creates and
-distributes an archive to your web servers, using the function deploy
-"""
-import os
-from datetime import datetime
-from fabric.api import *
+""" Fabric script (based on the file 2-do_deploy_web_static.py) that creates
+and distributes an archive to your web servers, using the function deploy: """
 
-env.hosts = ['52.3.247.21', '34.207.237.255']
+
+from fabric.api import *
+from datetime import datetime
+from os.path import exists
+# do_pack = __import__('1-pack_web_static').do_pack
+# do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+
+env.hosts = ['52.3.247.21', '34.207.237.255'] 
 
 def do_pack():
-    """Creates archive from web_static directory"""
-    local("mkdir -p versions")
-    file = 'versions/web_static_{}.tgz'\
-        .format(datetime.strftime(datetime.now(), "%Y%m%d%I%M%S"))
-    comp = 'tar -cvzf {} web_static'.format(file)
-    tar_file = local(comp)
-    if tar_file.failed:
-        return None
+    """generates a .tgz archive from the contents of the web_static folder
+    """
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
     else:
-        return file
+        return None
+
 
 def do_deploy(archive_path):
-    """Deploys an archive"""
-    if not os.path.exists(archive_path):
+    """ distributes an archive to my web servers
+    """
+    if exists(archive_path) is False:
+        return False  # Returns False if the file at archive_path doesnt exist
+    filename = archive_path.split('/')[-1]
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    # curr = '/data/web_static/current'
+    tmp = "/tmp/" + filename
+
+    try:
+        put(archive_path, "/tmp/")
+        # ^ Upload the archive to the /tmp/ directory of the web server
+        run("mkdir -p {}/".format(no_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+
+        return True
+    except:
         return False
 
-    arch = archive_path.split('/')[1]
-    name = arch.split('.')[0]
-
-    tar_file = put(archive_path, '/tmp/{}'.format(arch))
-    if tar_file.failed:
-        return False
-
-    tar_file = run('mkdir -p /data/web_static/releases/{}/'.format(name))
-    if tar_file.failed:
-        return False
-
-    tar_file = run(
-        'tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-        .format(arch, name))
-    if tar_file.failed:
-        return False
-
-    tar_file = run('rm /tmp/{}'.format(arch))
-    if tar_file.failed:
-        return False
-
-    comp = 'mv /data/web_static/releases/{0}/web_static/*'
-    comp += ' /data/web_static/releases/{0}/'
-    tar_file = run(comp.format(name))
-    if tar_file.failed:
-        return False
-
-    tar_file = run(
-        'rm -rf /data/web_static/releases/{}/web_static'
-        .format(name))
-    if tar_file.failed:
-        return False
-
-    # Use sudo for the commands that require elevated privileges
-    with settings(sudo_user='root'):
-        tar_file = sudo('rm -rf /data/web_static/current')
-        if tar_file.failed:
-            return False
-
-        tar_file = sudo(
-            'ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(name))
-        if tar_file.failed:
-            return False
-
-        # Add logic to create or copy my_index.html
-        tar_file = sudo(
-            'cp /data/web_static/releases/{0}/my_index.html /data/web_static/current/'
-            .format(name))
-        if tar_file.failed:
-            return False
-
-    print('New version deployed!')
-    return True
 
 def deploy():
-    """ Fabric script that creates and distributes an archive to your web servers,
-    using the function deploy"""
-    archive = do_pack()
-    if archive is None:
+    """ creates and distributes an archive to your web servers
+    """
+    new_archive_path = do_pack()
+    if exists(new_archive_path) is False:
         return False
-    tar_file = do_deploy(archive)
-    return tar_file
+    result = do_deploy(new_archive_path)
+    return result
