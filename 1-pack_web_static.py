9ac93730b13d5c7b20f086f63efbbf3221857b40
mmgmolotov <mmgmolotov@gmail.com>
2024-01-17 16:44:56 +0100

    Refactor do_pack function to improve file naming and add output messages

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-""" Compressed archive creation script """
+"""A module for Fabric script that generates a .tgz archive."""
+import os
+from datetime import datetime
+from fabric.api import local, runs_once
 
-from fabric.api import local
-import time
 
+@runs_once
 def do_pack():
-    """ Creates a compressed archive of web_static folder """
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    d_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        d_time.year,
+        d_time.month,
+        d_time.day,
+        d_time.hour,
+        d_time.minute,
+        d_time.second
+    )
     try:
-        current_time = time.strftime('%Y%m%d%H%M%S')
-        filename = "web_static_{}.tgz".format(current_time)
-        local("mkdir -p versions")
-        local("tar -cvzf versions/{} web_static/".format(filename))
-        return "versions/{}".format(filename)
-    except Exception as e:
-        print("An error occurred: {}".format(e))
-        return None
-
-# Calling the function
-do_pack()
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, size))
+    except Exception:
+        output = None
+    return output
